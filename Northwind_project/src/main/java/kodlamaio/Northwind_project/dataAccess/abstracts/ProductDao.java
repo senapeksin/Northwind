b99aab4cd28e961da0e92bc10501ed4cfdb4a3f0
SENA <senapksnn@gmail.com>
2021-06-01 19:33:19 +0300

    commit

--- Northwind_project/src/main/java/kodlamaio/Northwind_project/dataAccess/abstracts/ProductDao.java
+++ Northwind_project/src/main/java/kodlamaio/Northwind_project/dataAccess/abstracts/ProductDao.java
@@ -1,11 +1,29 @@
 package kodlamaio.Northwind_project.dataAccess.abstracts;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import kodlamaio.Northwind_project.entities.concretes.Product;
 
-
-
 public interface ProductDao extends JpaRepository<Product,Integer>{
+	 Product getByProductName(String productName);
+	 
+	 Product getByProductNameAndCategoryId(String productName,int categoryId);
+	 
+	 List<Product> getByProductNameOrCategoryId(String productName,int categoryId);
+	 
+	 List<Product> getByCategoryIdIn(List<Integer> categories);
+	 
+	 List<Product> getByProductNameContains(String productName);  // productName içeriyorsa
 
+	 List<Product> getByProductNameStartsWith(String productName);  //bu isimle başlayanları
+	 
+	 @Query("From Product where productName=:productName and categoryId =:categoryId")     //Query yazarken veritabanı tablosu olarak değil, entity olarak yazıyoruz.
+	 List<Product> getByNameAndCategory(String productName, int categoryId);
+	 // : 'den sonrası parametremizi veriyoruz.
+	 //select * from products where product_name= bisey and categoryId=bisey
+	 
+	 
 }
