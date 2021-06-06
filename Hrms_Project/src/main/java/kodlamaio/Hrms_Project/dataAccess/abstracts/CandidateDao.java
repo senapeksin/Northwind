feb1db561cbd5f4ad632f7b4e18dfcc47b49af6d
SENA <senapksnn@gmail.com>
2021-06-06 19:10:52 +0300

    commit

--- Hrms_Project/src/main/java/kodlamaio/Hrms_Project/dataAccess/abstracts/CandidateDao.java
+++
@@ -1,8 +0,0 @@
-package kodlamaio.Hrms_Project.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlamaio.Hrms_Project.entities.concretes.Candidate;
-
-public interface CandidateDao extends JpaRepository<Candidate, Integer> {
-}
--- Hrms_Project/src/main/java/kodlamaio/Hrms_Project/dataAccess/abstracts/JobTitleDao.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.Hrms_Project.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlamaio.Hrms_Project.entities.concretes.JobTitle;
-
-public interface JobTitleDao extends JpaRepository<JobTitle,Integer>{
-	
-}
